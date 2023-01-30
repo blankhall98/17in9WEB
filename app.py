fffd69180751105b4f75d851e4baa8e4e92529bb
blankhall98 <blankhall@ciencias.unam.mx>
2023-01-30 00:17:53 -0600

    main sections linked

--- app.py
+++ app.py
@@ -5,10 +5,27 @@ from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__)
 
 #ROUTES ----------
+
+### main pages
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/ods')
+def sdgs():
+    return render_template('sdg-base.html')
+
+@app.route('/limites')
+def limits():
+    return render_template('pb-base.html')
+
+@app.route('/modelo')
+def model():
+    return render_template('model-base.html')
+
+@app.route('/tesis')
+def thesis():
+    return render_template('thesis-base.html')
 
 #MAIN ----------
 def main():
