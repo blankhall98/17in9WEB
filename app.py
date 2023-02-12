7155a4de5b282c51720e0684b411417df91d692f
blankhall98 <blankhall@ciencias.unam.mx>
2023-02-11 20:44:06 -0600

    SDG templates ready <3

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 #IMPORTS ----------
 from flask import Flask, render_template, redirect, url_for
 
+### self-made imports
+from static.info.sdg.sdg import SDG
+
 #APP CONFIG ----------
 app = Flask(__name__)
 
@@ -26,6 +29,14 @@ def model():
 @app.route('/tesis')
 def thesis():
     return render_template('thesis-base.html')
+
+@app.route('/onwork')
+def onwork():
+    return render_template('onwork.html')
+
+@app.route('/SDG/<sdg_id>')
+def sdg(sdg_id):
+    return render_template('specific-sdg.html',sdg=SDG[sdg_id])
 
 #MAIN ----------
 def main():
---
+++ static/info/sdg/sdg.py
@@ -0,0 +1,160 @@
+SDG = {
+    '1': {
+        'long-title': "Poner fin a la pobreza en todas sus formas en todo el mundo",
+        'short-title': "Fin de la Pobreza",
+        'main-image': 'S_SDG_inverted_WEB-01',
+        "main-description": "A nivel mundial, el número de personas que viven en situación de extrema pobreza disminuyó desde un 36 % en 1990 hasta un 10 % en 2015. No obstante, el ritmo al que se produce este cambio está disminuyendo, y la crisis de la COVID-19 pone en riesgo décadas de progreso en la lucha contra la pobreza. Una nueva investigación publicada por el Instituto Mundial de Investigaciones de Economía del Desarrollo de la Universidad de las Naciones Unidas advierte de que las consecuencias económicas de la pandemia mundial podrían incrementar la pobreza en todo el mundo hasta llegar a afectar a 500 millones de personas más, o lo que es lo mismo, a un 8 % más de la población total mundial. Esta sería la primera vez que la pobreza aumente en todo el mundo en 30 años, desde 1990.",
+        'goals': [
+            "Para 2030, erradicar la pobreza extrema para todas las personas en el mundo, actualmente medida por un ingreso por persona inferior a 1,25 dólares al día.",
+            "Para 2030, reducir al menos a la mitad la proporción de hombres, mujeres y niños y niñas de todas las edades que viven en la pobreza en todas sus dimensiones con arreglo a las definiciones nacionales."
+        ],
+        "links": [
+            "http://www.undp.org/content/undp/es/home/",
+            "http://www.bancomundial.org/"
+        ]
+    },
+
+    '2': {
+        'long-title': 0,
+        'short-title': 0,
+        'main-image': 'S_SDG_inverted_WEB-02',
+        'main-description': 0,
+        'goals':[],
+        'links':[]
+    },
+
+    '3': {
+        'long-title': 0,
+        'short-title': 0,
+        'main-image': 'S_SDG_inverted_WEB-03',
+        'main-description': 0,
+        'goals':[],
+        'links':[]
+    },
+
+    '4': {
+        'long-title': 0,
+        'short-title': 0,
+        'main-image': 'S_SDG_inverted_WEB-04',
+        'main-description': 0,
+        'goals':[],
+        'links':[]
+    },
+
+    '5': {
+        'long-title': 0,
+        'short-title': 0,
+        'main-image': 'S_SDG_inverted_WEB-05',
+        'main-description': 0,
+        'goals':[],
+        'links':[]
+    },
+
+    '6': {
+        'long-title': 0,
+        'short-title': 0,
+        'main-image': 'S_SDG_inverted_WEB-06',
+        'main-description': 0,
+        'goals':[],
+        'links':[]
+    },
+
+    '7': {
+        'long-title': 0,
+        'short-title': 0,
+        'main-image': 'S_SDG_inverted_WEB-07',
+        'main-description': 0,
+        'goals':[],
+        'links':[]
+    },
+
+    '8': {
+        'long-title': 0,
+        'short-title': 0,
+        'main-image': 'S_SDG_inverted_WEB-08',
+        'main-description': 0,
+        'goals':[],
+        'links':[]
+    },
+
+    '9': {
+        'long-title': 0,
+        'short-title': 0,
+        'main-image': 'S_SDG_inverted_WEB-09',
+        'main-description': 0,
+        'goals':[],
+        'links':[]
+    },
+
+    '10': {
+        'long-title': 0,
+        'short-title': 0,
+        'main-image': 'S_SDG_inverted_WEB-10',
+        'main-description': 0,
+        'goals':[],
+        'links':[]
+    },
+
+    '11': {
+        'long-title': 0,
+        'short-title': 0,
+        'main-image': 'S_SDG_inverted_WEB-11',
+        'main-description': 0,
+        'goals':[],
+        'links':[]
+    },
+    
+    '12': {
+        'long-title': 0,
+        'short-title': 0,
+        'main-image': 'S_SDG_inverted_WEB-12',
+        'main-description': 0,
+        'goals':[],
+        'links':[]
+    },
+
+    '13': {
+        'long-title': 0,
+        'short-title': 0,
+        'main-image': 'S_SDG_inverted_WEB-13',
+        'main-description': 0,
+        'goals':[],
+        'links':[]
+    },
+
+    '14': {
+        'long-title': 0,
+        'short-title': 0,
+        'main-image': 'S_SDG_inverted_WEB-14',
+        'main-description': 0,
+        'goals':[],
+        'links':[]
+    },
+
+    '15': {
+        'long-title': 0,
+        'short-title': 0,
+        'main-image': 'S_SDG_inverted_WEB-15',
+        'main-description': 0,
+        'goals':[],
+        'links':[]
+    },
+
+    '16': {
+        'long-title': 0,
+        'short-title': 0,
+        'main-image': 'S_SDG_inverted_WEB-16',
+        'main-description': 0,
+        'goals':[],
+        'links':[]
+    },
+
+    '17': {
+        'long-title': 0,
+        'short-title': 0,
+        'main-image': 'S_SDG_inverted_WEB-17',
+        'main-description': 0,
+        'goals':[],
+        'links':[]
+    }
+}
