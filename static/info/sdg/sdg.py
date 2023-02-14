cb067393844720fbfc46a3a1d3a799df621fa8b1
blankhall98 <blankhall@ciencias.unam.mx>
2023-02-14 16:25:56 -0600

    final details sdg not filled with info yet

--- static/info/sdg/sdg.py
+++ static/info/sdg/sdg.py
@@ -3,10 +3,16 @@ SDG = {
         'long-title': "Poner fin a la pobreza en todas sus formas en todo el mundo",
         'short-title': "Fin de la Pobreza",
         'main-image': 'S_SDG_inverted_WEB-01',
-        "main-description": "A nivel mundial, el número de personas que viven en situación de extrema pobreza disminuyó desde un 36 % en 1990 hasta un 10 % en 2015. No obstante, el ritmo al que se produce este cambio está disminuyendo, y la crisis de la COVID-19 pone en riesgo décadas de progreso en la lucha contra la pobreza. Una nueva investigación publicada por el Instituto Mundial de Investigaciones de Economía del Desarrollo de la Universidad de las Naciones Unidas advierte de que las consecuencias económicas de la pandemia mundial podrían incrementar la pobreza en todo el mundo hasta llegar a afectar a 500 millones de personas más, o lo que es lo mismo, a un 8 % más de la población total mundial. Esta sería la primera vez que la pobreza aumente en todo el mundo en 30 años, desde 1990.",
+        "main-description": [
+            "La pobreza es una situación en la cual no es posible satisfacer las necesidades físicas y psicológicas básicas de una persona debido a falta de recursos como: la alimentación, la vivienda, la educación, la asistencia sanitaria, el agua potable o la electricidad",
+            "El aumento de la desigualdad es perjudicial para el crecimiento económico y socava la cohesión social, lo que incrementa las tensiones políticas y sociales y, en algunos casos, impulsa la inestabilidad y los conflictos.",
+            "El enfoque más común define la pobreza como falta de ingresos; la pobreza se configura cuando el total de ingresos disponibles no satisface el mínimo necesario para la subsistencia. En 2015, el Banco clasifica como pobre una persona que vive con menos de $1,90 Dólares Americanos al día.",
+            "A nivel mundial, el número de personas que viven en una situación de pobreza extrema disminuyó desde un 36 % en 1990 hasta un 10 % en 2015. Es sumamente importante recalcar que gran parte de este monumental progreso se debe al progreso logrado por China en las últimas dos décadas - en 40 años alrededor de 770 millones de habitantes chinos salieron de la pobreza. Más de 700 millones de personas, o el 10 % de la población total mundial aún vive en situación de pobreza extrema a día de hoy. La mayoría de las personas que viven con menos de 1,90 dólares al día - el 70 % - viven en dos regiones: África Subsahariana y Asia meridional."
+        ],
         'goals': [
             "Para 2030, erradicar la pobreza extrema para todas las personas en el mundo, actualmente medida por un ingreso por persona inferior a 1,25 dólares al día.",
-            "Para 2030, reducir al menos a la mitad la proporción de hombres, mujeres y niños y niñas de todas las edades que viven en la pobreza en todas sus dimensiones con arreglo a las definiciones nacionales."
+            "Para 2030, reducir al menos a la mitad la proporción de hombres, mujeres y niños y niñas de todas las edades que viven en pobreza en todas sus dimensiones con arreglo a las definiciones nacionales.",
+            "Poner en práctica a nivel nacional sistemas y medidas apropiadas de protección social para todos y, para 2030, lograr una amplia cobertura de los pobres y los más vulnerables."
         ],
         "links": [
             "http://www.undp.org/content/undp/es/home/",
@@ -15,145 +21,145 @@ SDG = {
     },
 
     '2': {
-        'long-title': 0,
-        'short-title': 0,
+        'long-title': 'Poner fin al hambre',
+        'short-title': 'Hambre Cero',
         'main-image': 'S_SDG_inverted_WEB-02',
-        'main-description': 0,
+        'main-description': [],
         'goals':[],
         'links':[]
     },
 
     '3': {
-        'long-title': 0,
-        'short-title': 0,
+        'long-title': 'Garantizar una vida sana y promover el bienestar para todos en todas las edades',
+        'short-title': 'Salud y Bienestar',
         'main-image': 'S_SDG_inverted_WEB-03',
-        'main-description': 0,
+        'main-description': [],
         'goals':[],
         'links':[]
     },
 
     '4': {
-        'long-title': 0,
-        'short-title': 0,
+        'long-title': 'Garantizar una educación inclusiva, equitativa y de calidad y promover oportunidades de aprendizaje durante toda la vida para todos',
+        'short-title': 'Educación de Calidad',
         'main-image': 'S_SDG_inverted_WEB-04',
-        'main-description': 0,
+        'main-description': [],
         'goals':[],
         'links':[]
     },
 
     '5': {
-        'long-title': 0,
-        'short-title': 0,
+        'long-title': 'Lograr la igualdad entre los géneros y empoderar a todas las mujeres y las niñas',
+        'short-title': 'Igualdad de Género',
         'main-image': 'S_SDG_inverted_WEB-05',
-        'main-description': 0,
+        'main-description': [],
         'goals':[],
         'links':[]
     },
 
     '6': {
-        'long-title': 0,
-        'short-title': 0,
+        'long-title': 'Garantizar la disponibilidad de agua y su gestión sostenible y el saneamiento para todos',
+        'short-title': 'Agua Limpia y Saneamiento',
         'main-image': 'S_SDG_inverted_WEB-06',
-        'main-description': 0,
+        'main-description': [],
         'goals':[],
         'links':[]
     },
 
     '7': {
-        'long-title': 0,
-        'short-title': 0,
+        'long-title': 'Garantizar el acceso a una energía asequible, segura, sostenible y moderna',
+        'short-title': 'Energía Asequible y No Contaminante',
         'main-image': 'S_SDG_inverted_WEB-07',
-        'main-description': 0,
+        'main-description': [],
         'goals':[],
         'links':[]
     },
 
     '8': {
-        'long-title': 0,
-        'short-title': 0,
+        'long-title': 'Promover el crecimiento económico inclusivo y sostenible, el empleo y el trabajo decente para todos',
+        'short-title': 'Trabajo Decente y Crecimiento Económico',
         'main-image': 'S_SDG_inverted_WEB-08',
-        'main-description': 0,
+        'main-description': [],
         'goals':[],
         'links':[]
     },
 
     '9': {
-        'long-title': 0,
-        'short-title': 0,
+        'long-title': 'Construir infraestructuras resilientes, promover la industrialización sostenible y fomentar la innovación',
+        'short-title': 'Industria, Innovación e Infraestructuras',
         'main-image': 'S_SDG_inverted_WEB-09',
-        'main-description': 0,
+        'main-description': [],
         'goals':[],
         'links':[]
     },
 
     '10': {
-        'long-title': 0,
-        'short-title': 0,
+        'long-title': 'Reducir la desigualdad en y entre los países',
+        'short-title': 'Reducción de las Desigualdades',
         'main-image': 'S_SDG_inverted_WEB-10',
-        'main-description': 0,
+        'main-description': [],
         'goals':[],
         'links':[]
     },
 
     '11': {
-        'long-title': 0,
-        'short-title': 0,
+        'long-title': 'Lograr que las ciudades sean más inclusivas, seguras, resilientes y sostenibles',
+        'short-title': 'Ciudades y Comunidades Sostenibles',
         'main-image': 'S_SDG_inverted_WEB-11',
-        'main-description': 0,
+        'main-description': [],
         'goals':[],
         'links':[]
     },
     
     '12': {
-        'long-title': 0,
-        'short-title': 0,
+        'long-title': 'Garantizar modalidades de consumo y producción sostenibles',
+        'short-title': 'Producción y Consumo Responsables',
         'main-image': 'S_SDG_inverted_WEB-12',
-        'main-description': 0,
+        'main-description': [],
         'goals':[],
         'links':[]
     },
 
     '13': {
-        'long-title': 0,
-        'short-title': 0,
+        'long-title': 'Adoptar medidas urgentes para combatir el cambio climático y sus efectos',
+        'short-title': 'Acción por el Clima',
         'main-image': 'S_SDG_inverted_WEB-13',
-        'main-description': 0,
+        'main-description': [],
         'goals':[],
         'links':[]
     },
 
     '14': {
-        'long-title': 0,
-        'short-title': 0,
+        'long-title': 'Conservar y utilizar sosteniblemente los océanos, los mares y los recursos marinos',
+        'short-title': 'Vida Submarina',
         'main-image': 'S_SDG_inverted_WEB-14',
-        'main-description': 0,
+        'main-description': [],
         'goals':[],
         'links':[]
     },
 
     '15': {
-        'long-title': 0,
-        'short-title': 0,
+        'long-title': 'Gestionar sosteniblemente los bosques, luchar contra la desertificación, detener e invertir la degradación de las tierras, detener la pérdida de biodiversidad',
+        'short-title': 'Vida de Ecosistemas Terrestres',
         'main-image': 'S_SDG_inverted_WEB-15',
-        'main-description': 0,
+        'main-description': [],
         'goals':[],
         'links':[]
     },
 
     '16': {
-        'long-title': 0,
-        'short-title': 0,
+        'long-title': 'Promover sociedades justas, pacíficas e inclusivas',
+        'short-title': 'Paz, Justicia e Instituciones Sólidas',
         'main-image': 'S_SDG_inverted_WEB-16',
-        'main-description': 0,
+        'main-description': [],
         'goals':[],
         'links':[]
     },
 
     '17': {
-        'long-title': 0,
-        'short-title': 0,
+        'long-title': 'Revitalizar la Alianza Mundial por el Desarrollo Sostenible',
+        'short-title': 'Alianzas para Lograr los Objetivos',
         'main-image': 'S_SDG_inverted_WEB-17',
-        'main-description': 0,
+        'main-description': [],
         'goals':[],
         'links':[]
     }
